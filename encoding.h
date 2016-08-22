348f0168cae97933a433a11459de95e75d94c743
Tim Newsome <tim@sifive.com>
2016-08-22 14:15:18 -0700

    Make hardware triggers match latest spec.

--- encoding.h
+++ encoding.h
@@ -57,6 +57,32 @@
 #define DCSR_CAUSE_STEP     4
 #define DCSR_CAUSE_HALT     5
 
+#define MCONTROL_SELECT     (1<<19)
+#define MCONTROL_ACTION     (0x7f<<12)
+#define MCONTROL_CHAIN      (1<<11)
+#define MCONTROL_MATCH      (0xf<<7)
+#define MCONTROL_M          (1<<6)
+#define MCONTROL_H          (1<<5)
+#define MCONTROL_S          (1<<4)
+#define MCONTROL_U          (1<<3)
+#define MCONTROL_EXECUTE    (1<<2)
+#define MCONTROL_STORE      (1<<1)
+#define MCONTROL_LOAD       (1<<0)
+
+#define MCONTROL_ACTION_NONE	        0
+#define MCONTROL_ACTION_DEBUG_EXCEPTION	1
+#define MCONTROL_ACTION_DEBUG_MODE	2
+#define MCONTROL_ACTION_TRACE_START	3
+#define MCONTROL_ACTION_TRACE_STOP	4
+#define MCONTROL_ACTION_TRACE_EMIT	5
+
+#define MCONTROL_MATCH_EQUAL	        0
+#define MCONTROL_MATCH_NAPOT	        1
+#define MCONTROL_MATCH_GE	        2
+#define MCONTROL_MATCH_LT	        3
+#define MCONTROL_MATCH_MASK_LOW	        4
+#define MCONTROL_MATCH_MASK_HIGH	5
+
 #define MIP_SSIP            (1 << IRQ_S_SOFT)
 #define MIP_HSIP            (1 << IRQ_H_SOFT)
 #define MIP_MSIP            (1 << IRQ_M_SOFT)
