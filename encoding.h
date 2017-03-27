d86c50a5a6f44ed4b0e5506dbbd782fed3f1fedd
Andrew Waterman <andrew@sifive.com>
2017-03-27 14:25:34 -0700

    Separate page faults from physical memory access exceptions

--- encoding.h
+++ encoding.h
@@ -147,9 +147,8 @@
 #define IRQ_HOST     13
 
 #define DEFAULT_RSTVEC     0x00001000
-#define DEFAULT_NMIVEC     0x00001004
-#define DEFAULT_MTVEC      0x00001010
-#define CONFIG_STRING_ADDR 0x0000100C
+#define CLINT_BASE         0x02000000
+#define CLINT_SIZE         0x000c0000
 #define EXT_IO_BASE        0x40000000
 #define DRAM_BASE          0x80000000
 
