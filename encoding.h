85c4a2c091945d5c040f84c88191748d005844a6
Dan Petrisko <petrisko@cs.washington.edu>
2021-01-23 19:38:45 -0800

    Removing platform-specific definitions

--- encoding.h
+++ encoding.h
@@ -206,12 +206,6 @@
 #define IRQ_COP      12
 #define IRQ_HOST     13
 
-#define DEFAULT_RSTVEC     0x00001000
-#define CLINT_BASE         0x02000000
-#define CLINT_SIZE         0x000c0000
-#define EXT_IO_BASE        0x40000000
-#define DRAM_BASE          0x80000000
-
 /* page table entry (PTE) fields */
 #define PTE_V     0x001 /* Valid */
 #define PTE_R     0x002 /* Read */
