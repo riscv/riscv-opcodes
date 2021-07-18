d936c34654e6e000026d95bb540c160399d1bd56
Dan Lustig <dlustig@nvidia.com>
2021-07-18 16:50:28 -0400

    Virtual memory updates
    * Add Svinval instructions
    * Add PTE defines for Priv 1.12 and Svpbmt

--- encoding.h
+++ encoding.h
@@ -216,7 +216,10 @@
 #define PTE_A     0x040 /* Accessed */
 #define PTE_D     0x080 /* Dirty */
 #define PTE_SOFT  0x300 /* Reserved for Software */
+#define PTE_RSVD  0x1FC0000000000000 /* Reserved for future standard use */
+#define PTE_PBMT  0x6000000000000000 /* Svpbmt: Page-based memory types */
 #define PTE_N     0x8000000000000000 /* Svnapot: NAPOT translation contiguity */
+#define PTE_ATTR  0xFFC0000000000000 /* All attributes and reserved bits */
 
 #define PTE_PPN_SHIFT 10
 
