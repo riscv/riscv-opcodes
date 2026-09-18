bc5cb316165adde4448108a0ee2658ea1baa3f5e
Zeyi Xu <mitchell.xu2@gmail.com>
2026-09-18 13:22:44 +0800

    Fix operand fields for dot-product instructions
    Signed-off-by: Zeyi Xu <mitchell.xu2@gmail.com>

--- src/riscv_opcodes/constants.py
+++ src/riscv_opcodes/constants.py
@@ -96,6 +96,8 @@ latex_mapping = {
     "rs1": "rs1",
     "rs2": "rs2",
     "rd": "rd",
+    "vs2_bdot": "vs2[4:3]",
+    "ci": "ci[5:3]",
     "imm20": "imm[31:12]",
     "bimm12hi": "imm[12$\\vert$10:5]",
     "bimm12lo": "imm[4:1$\\vert$11]",
